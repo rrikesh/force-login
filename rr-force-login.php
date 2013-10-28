37e7d40262d1f24097ebd7d08d3cdb798d5967bc
Rikesh Ramlochund <rrikesh@gmail.com>
2013-10-28 10:59:38 -0700

    Added plugin file

---
+++ rr-force-login.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+   Plugin name: Force Login
+   Author: Rikesh Ramlochund
+   Author URI: http://rrikesh.com
+   Description: Plugin that restricts access only to logged-in users. Can be used as a must-use plugin too.
+   Version: 1.0
+*/
+
+class RR_ForceLogin{
+
+function __construct(){
+  add_action( 'template_redirect', array( $this, 'redirect_user_to_the_login_page' ) );
+}
+
+/**
+ * Force users not logged in to the login page
+ */
+function redirect_user_to_the_login_page() {
+  if ( !is_user_logged_in() ) {
+    wp_redirect( wp_login_url(), 302 );
+    exit;
+  }
+}
+
+}
+
+new RR_ForceLogin();
